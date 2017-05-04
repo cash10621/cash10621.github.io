1b1ee13cfec7e3e42e11bfc5ec6e2b9f0f1ac979
cash10621 <cash10621@gmail.com>
2017-05-04 23:35:26 +0800

    6666

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,2 @@
-var randomnumber=Math.random()*5;importScripts('//cdn.izooto.com/scripts/workers/2fcac0831501e23934f23a620f112f9919090739.js?'+randomnumber);
+var randomnumber = Math.random() * 5;
+importScripts('//cdn.izooto.com/scripts/workers/2fcac0831501e23934f23a620f112f9919090739.js?' + randomnumber);
