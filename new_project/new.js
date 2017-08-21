9951c0fa334fd3502fad94a90fd2d48bdfee962c
cash10621 <cash10621@gmail.com>
2017-08-21 22:56:27 +0800

    654

--- new_project/new.js
+++ new_project/new.js
@@ -19,12 +19,55 @@ position = moveFord(10);
 console.log(position);
 
 $(function () {
-    var $button = $("button");
+    //   $("#meg").hide();
+    var $button = $("#login");
     $button.click(
         function () {
             var $result = $("#meg");
             var $nameInput = $("#input");
             var name = $nameInput.val();
             $result.text("hello" + name);
+            $result.show();
         });
 });
+
+
+function Pokemon(
+    number, type, height, weight, abilities, japanese
+) {
+    this.number = number;
+    this.type = type;
+    this.height = height;
+    this.weight = weight;
+    this.abilities = abilities;
+    this.japanese = japanese;
+};
+
+var type_html_start = '<buttom class="btn btn-default">';
+var type_html_end = '</buttom>';
+
+// 宣告變數不能用大寫頭
+var pokemon = new Pokemon(132, ["cool", "big"], 1.6, 1.2, "speeh", "sanda");
+var pokemon1 = new Pokemon(133, ["cool", "small"], 1.0, 2.2, "speeh", "undefind");
+
+
+$(function () {
+    $("#number").text(pokemon.number);
+    $("#type").html(type_html_start + pokemon.type + type_html_end);
+    $("#height").text(pokemon.height);
+    $("#weight").text(pokemon.weight);
+    $("#abilities").text(pokemon.abilities);
+    $("#japanese").text(pokemon.japanese);
+    $("#meg").html("Hello <b>world</b>!");
+});
+
+
+// 用function定義類別(第一個字大寫)
+function Student(name, phone, city) {
+    this.name = name;
+    this.phone = phone;
+    this.city = city;
+};
+var student1 = new Sutdent("aaa", "0917534512", "taipei");
+var student2 = new Sutdent("bbb", "0917534545", "taipei");
+// END--- 用function定義類別
